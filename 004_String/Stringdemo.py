dda3f9e21104b784d4417b659d4f8053643f099a
chintan <chintan@gmail.com>
2026-01-09 10:48:20 +0530

    all

---
+++ 004_String/Stringdemo.py
@@ -0,0 +1,31 @@
+# st = "sun rises In East ß µ ς "
+
+# print(len(st))
+# print(st.lower())
+# print(st.casefold())
+# print(st.title())
+# print(st.capitalize())
+# print(st.upper())
+# print(st.strip())
+# print(st.replace('s','K',2))
+# print(st.find('Ea'))
+# print(st.startswith("s"))
+# print(st.endswith("e"))
+# print(st.split(" ",2))
+# print(st.join("XYZ"))
+# print("abc222".isalpha())
+# print("22121eee".isdigit())
+# print("ssbs11$".isalnum())
+# print("abc".zfill(10))
+# print("abc".center(12,'*'))
+
+
+st = "Hello python hello tops"
+
+print(st[0:7])
+print(st[2:8])
+print(st[:8])
+print(st[5:])
+print(st[1:17:3])
+print(st[-5:-2])
+print(st[::-1])
---
+++ 004_String/Stringpracticals.py
@@ -0,0 +1,4 @@
+st = "sun rises in east"
+
+#east in rises sun
+#nus sesir ni tsae
