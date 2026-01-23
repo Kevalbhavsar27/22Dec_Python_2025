0118d35a3dd791e5f83709f6dd61ee91a8321c8a
chintan <chintan@gmail.com>
2026-01-23 10:56:42 +0530

    all

---
+++ 006_Function/FunDemo.py
@@ -0,0 +1,60 @@
+# def test():
+#     print("Hello")
+# test()
+
+
+# def square(a):
+#     print(a*a)
+# square(10)
+# square(20)
+
+# def sum(a,b):
+#     print(f"sum of {a} and {b} is {a+b}")
+# sum(10,20)
+# sum(50,60)
+
+# def total(a,b,c):
+#     add = a+b+c
+#     return add
+
+# def per(total):
+#     pr = (total*100) / 150
+#     print(pr)
+
+# add = total(10,20,30)
+# per(add)
+
+# def test():
+    # print("test")
+    # return "abc"
+
+# k = test()
+# print(k)
+# print(test())
+
+
+
+# def person(name,email="test@gmial.com",phone=7485968574):
+#     print(name,email,phone)
+
+# person("Ashish",phone = 9685748596)
+
+
+# def total(*a):
+#     sum = 0
+#     for i in a:
+#         sum+=i
+#     print(sum)
+# total(10,20,30,40,45,63)
+
+# def student(**a):
+#     print(a)
+
+# student(name="abc",email="abc@gmail.com")
+
+
+# def mul(a,b):
+#     return a*b
+# m = lambda a,b : a*b
+# print(m(10,20))
+
---
+++ 006_Function/Recursion.py
@@ -0,0 +1,8 @@
+
+def square(a):
+    print(a*a)
+    a+=1
+    if a<=10:
+        square(a)
+
+square(1)
