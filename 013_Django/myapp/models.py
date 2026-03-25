9e3364cb7075f077363f5c9a5e928b8b32654784
chintan <chintan@gmail.com>
2026-03-25 11:00:03 +0530

    crud

--- 013_Django/myapp/models.py
+++ 013_Django/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    age = models.IntegerField()
+    dob = models.DateField()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    qty = models.IntegerField()
