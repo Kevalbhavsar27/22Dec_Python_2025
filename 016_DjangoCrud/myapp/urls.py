1685720c53ba589bdf1f9e132c356497d386de8f
chintan <chintan@gmail.com>
2026-04-01 10:05:40 +0530

    all

--- 016_DjangoCrud/myapp/urls.py
+++ 016_DjangoCrud/myapp/urls.py
@@ -4,5 +4,6 @@ from myapp.views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("delete",delete,name="delete")
+    path("delete",delete,name="delete"),
+    path("update",update,name="update")
 ]
--- 016_DjangoCrud/myapp/views.py
+++ 016_DjangoCrud/myapp/views.py
@@ -20,4 +20,23 @@ def delete(request):
     st = Student.objects.get(id=did)
     st.delete()
     return redirect("index")
-    
+
+def update(request):
+    all = Student.objects.all()
+    if request.method=='POST':
+        data = request.POST
+        id = data.get("id")
+        name = data.get("name")
+        email = data.get("email")
+        age = data.get("age")
+
+        st = Student.objects.get(id=id)
+        st.name = name
+        st.email = email
+        st.age = age
+        st.save()
+        
+        return render(request,"index.html",{"msg":"update suceess","students":all})  
+    uid = request.GET['uid']
+    st = Student.objects.get(id=uid)
+    return render(request,"index.html",{"student":st,"students":all})
